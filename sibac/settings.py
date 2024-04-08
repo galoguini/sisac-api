505dec19a4a6a4e7480dab56352e34b0c9bb3c9f
galoguini <galtahgui.ni@gmail.com>
2024-04-07 21:19:07 -0300

    Docs y requerimientos
    -Agregado de auto documentación con coreapi
    -Agregado archivo de requerimientos del entorno

--- sibac/settings.py
+++ sibac/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'corsheaders',
+    'coreapi',
     'usuarios',
 ]
 
@@ -131,8 +132,10 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
+
+    "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema",
 }
 
 CORS_ALLOWED_ORIGINS = [
-
+    "http://localhost:5173",
 ]
--- sibac/urls.py
+++ sibac/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from rest_framework.documentation import include_docs_urls
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('docs/', include_docs_urls(title='SIBAC API')),
     path('api/v1/usuarios/', include('usuarios.urls')),
 ]
