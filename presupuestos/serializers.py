57051d7a7ba1197b6ca978950448faf786a8deba
fac1L <galtahgui.ni@gmail.com>
2024-06-11 20:38:41 -0300

    limpieza de codigo
    .

--- presupuestos/serializers.py
+++ presupuestos/serializers.py
@@ -1,89 +1,3 @@
-# from rest_framework import serializers
-# from .models import Presupuesto, PresupuestoProducto
-# from clientes.models import Cliente
-# from productos.models import Producto  
-# from clientes.serializers import ClienteSerializer
-# from productos.serializers import ProductoSerializer
-
-# class ProductoPresupuestoSerializer(serializers.ModelSerializer):
-#     producto = ProductoSerializer(read_only=True)
-#     producto_id = serializers.IntegerField(write_only=True)
-
-#     class Meta:
-#         model = PresupuestoProducto
-#         fields = ['producto', 'producto_id', 'cantidad', 'precio']
-
-#     def to_representation(self, instance):
-#         representation = super().to_representation(instance)
-#         representation['producto'] = ProductoSerializer(instance.producto).data
-#         return representation
-
-# class PresupuestoSerializer(serializers.ModelSerializer):
-#     cliente = ClienteSerializer(read_only=True)
-#     productos = ProductoPresupuestoSerializer(many=True)
-
-#     def __init__(self, *args, **kwargs):
-#         super().__init__(*args, **kwargs)
-#         request = self.context.get('request')
-#         if request and hasattr(request, "user"):
-#             self.fields['cliente'].queryset = Cliente.objects.filter(usuario=request.user)
-#             self.fields['productos'].queryset = PresupuestoProducto.objects.filter(presupuesto__usuario=request.user)
-
-#     class Meta:
-#         model = Presupuesto
-#         fields = ['id', 'cliente', 'fecha', 'vencimiento', 'moneda', 'productos', 'observaciones', 'numero_presupuesto']
-
-#     def create(self, validated_data):
-#         productos_data = validated_data.pop('productos')
-#         presupuesto = Presupuesto.objects.create(**validated_data)
-#         for producto_data in productos_data:
-#             producto_id = producto_data.pop('producto_id')
-#             producto = Producto.objects.get(id=producto_id)
-#             PresupuestoProducto.objects.create(presupuesto=presupuesto, producto=producto, **producto_data)
-#         return presupuesto
-
-
-# serializers.py
-# from rest_framework import serializers
-# from .models import Presupuesto, PresupuestoProducto
-# from productos.models import Producto
-
-# class PresupuestoProductoSerializer(serializers.ModelSerializer):
-#     producto = serializers.PrimaryKeyRelatedField(queryset=Producto.objects.all())
-
-#     class Meta:
-#         model = PresupuestoProducto
-#         fields = ['id' ,'producto', 'cantidad', 'precio']
-
-# class PresupuestoSerializer(serializers.ModelSerializer):
-#     productos = PresupuestoProductoSerializer(source='presupuestoproducto_set', many=True)
-
-#     class Meta:
-#         model = Presupuesto
-#         fields = ['id', 'cliente', 'fecha', 'vencimiento', 'moneda', 'observaciones', 'productos']
-
-#     def create(self, validated_data):
-#         productos_data = validated_data.pop('productos')
-#         presupuesto = Presupuesto.objects.create(**validated_data)
-#         for producto_data in productos_data:
-#             producto_id = producto_data.pop('producto').id  # Esto ya es el ID del producto
-#             producto = Producto.objects.get(id=producto_id)
-#             PresupuestoProducto.objects.create(presupuesto=presupuesto, producto=producto, **producto_data)
-#         return presupuesto
-
-
-# class PresupuestoProductoSerializer(serializers.ModelSerializer):
-#     producto = serializers.SlugRelatedField(
-#         slug_field='nombre',
-#         queryset=Producto.objects.all()
-#     )
-
-#     class Meta:
-#         model = PresupuestoProducto
-#         fields = ['producto', 'cantidad', 'precio']
-
-
-
 from rest_framework import serializers
 from .models import Presupuesto, Cliente, Producto, PresupuestoProducto
 from django.core.exceptions import ObjectDoesNotExist
@@ -126,18 +40,7 @@ class PresupuestoProductoSerializer(serializers.ModelSerializer):
         model = PresupuestoProducto
         fields = ['producto', 'cantidad', 'precio', 'descripcion']
 
-# class PresupuestoProductoSerializer(serializers.ModelSerializer):
-#     producto = serializers.SlugRelatedField(slug_field='nombre', queryset=Producto.objects.all(),)
-
-#     class Meta:
-#         model = PresupuestoProducto
-#         fields = ['producto', 'cantidad', 'precio']
-
 class PresupuestoSerializer(serializers.ModelSerializer):
-    # cliente = serializers.SlugRelatedField(
-    #     slug_field='nombre_apellido',  
-    #     queryset=Cliente.objects.all(),  
-    # )
     cliente = ClienteField(queryset=Cliente.objects.all())
     productos = PresupuestoProductoSerializer(many=True)
 
--- presupuestos/views.py
+++ presupuestos/views.py
@@ -1,60 +1,9 @@
 from django_filters import rest_framework
-from rest_framework import generics, permissions
-# from .models import Presupuesto, PresupuestoProducto
-# from .serializers import PresupuestoSerializer
+from rest_framework import generics, permissions, status
 from .filters import PresupuestoFilter
-# from usuarios.models import Usuario
-from rest_framework import serializers
-# from productos.models import Producto
-
-# class ListarPresupuestosView(generics.ListAPIView):
-#     serializer_class = PresupuestoSerializer
-#     permission_classes = [permissions.IsAuthenticated]
-#     filter_backends = [rest_framework.DjangoFilterBackend]
-#     filterset_class = PresupuestoFilter
-
-#     def get_queryset(self):
-#         user = self.request.user
-#         return Presupuesto.objects.filter(usuario=user)
-
-# class AgregarPresupuestoView(generics.CreateAPIView):
-#     queryset = Presupuesto.objects.all()
-#     serializer_class = PresupuestoSerializer
-#     permission_classes = [permissions.IsAuthenticated]
-
-#     def perform_create(self, serializer):
-#         usuario = self.request.user
-#         empresa = usuario.empresa_seleccionada
-#         print(f'Saving presupuesto for user: {usuario.id} and empresa: {empresa.id}')
-#         serializer.save(usuario=usuario, empresa=empresa)
-
-# class EditarPresupuestoView(generics.UpdateAPIView):
-#     queryset = Presupuesto.objects.all()
-#     serializer_class = PresupuestoSerializer
-#     permission_classes = [permissions.IsAuthenticated]
-
-# class EliminarPresupuestoView(generics.DestroyAPIView):
-#     queryset = Presupuesto.objects.all()
-#     serializer_class = PresupuestoSerializer
-#     permission_classes = [permissions.IsAuthenticated]
-#     lookup_field = 'numero_presupuesto'
-
-# class DetallePresupuestoView(generics.RetrieveAPIView):
-#     queryset = Presupuesto.objects.all()
-#     serializer_class = PresupuestoSerializer
-#     permission_classes = [permissions.IsAuthenticated]
-#     lookup_field = 'numero_presupuesto'
-
-# class ImpriPresupuestoView(generics.RetrieveAPIView):
-#     queryset = Presupuesto.objects.all()
-#     serializer_class = PresupuestoSerializer
-#     permission_classes = [permissions.IsAuthenticated]
-#     lookup_field = 'numero_presupuesto'
-
 from rest_framework import generics, permissions
 from .models import Presupuesto
 from .serializers import PresupuestoSerializer
-from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
