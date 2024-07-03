2d63056433615263635e59fc93b1ec56aa575bda
Galo Tahiel Guini <galtahgui.ni@gmail.com>
2024-07-03 17:25:48 -0300

    corrección del admin

--- usuarios/middleware.py
+++ usuarios/middleware.py
@@ -5,7 +5,14 @@ class EmpresaSeleccionadaMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
+        # Verificar si la solicitud está dirigida al admin de Django
+        if request.path.startswith('/admin'):
+            # Si es así, simplemente pasa la solicitud al siguiente middleware o vista sin hacer nada
+            return self.get_response(request)
+
+        # Tu lógica existente para usuarios autenticados fuera del admin
         if request.user.is_authenticated and not request.user.empresa_seleccionada:
             return Response({'error': 'Debe seleccionar o crear una empresa.'}, status=400)
+        
         response = self.get_response(request)
         return response
