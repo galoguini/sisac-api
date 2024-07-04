c38df657e6ac807db876a8def1d62b51a9632e3c
Galo Tahiel Guini <galtahgui.ni@gmail.com>
2024-07-03 21:19:46 -0300

    corrección de entrega de imágenes

--- sibac/settings.py
+++ sibac/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'https://web-production-d54a.up.railway.app/static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
@@ -150,7 +150,7 @@ CORS_ALLOWED_ORIGINS = [
 
 AUTH_USER_MODEL = 'usuarios.Usuario'
 
-MEDIA_URL = 'https://web-production-d54a.up.railway.app/media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
