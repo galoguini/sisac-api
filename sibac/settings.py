356fd640a5e154be3d488eabd6c9df0533b6b6ee
Galo Tahiel Guini <galtahgui.ni@gmail.com>
2024-07-08 22:16:30 -0300

    corrección para soportar múltiples cors

--- sibac/settings.py
+++ sibac/settings.py
@@ -144,7 +144,7 @@ REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema",
 }
 
-CORS_ALLOWED_ORIGINS = [os.getenv('CORS_ALLOWED_ORIGINS')]
+CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS", "").split(",")
 
 AUTH_USER_MODEL = 'usuarios.Usuario'
 
